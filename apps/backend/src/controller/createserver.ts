7d45dd7058de52994658848bd75fbae96ab80d7d
tanzim_safin <tanzimhossainsafin756@gmail.com>
2026-01-01 21:31:30 +0600

    added the simple version of controller logic/ business logic for Create Server and joinServer

--- apps/backend/src/controller/createserver.ts
+++ apps/backend/src/controller/createserver.ts
@@ -1,3 +1,10 @@
-export function createServer() {
-    console.log("Create Server Controller");
-}
+import { Request, Response } from "express";
+import CreateServer from "../models/createServer.model";
+export const createServer=async(req:Request,res:Response)=>{
+    const data=req.body;
+    if(!data){
+        return res.status(400).json({message:"Bad Request"});
+    };
+    const server=await CreateServer.create(data);
+    return res.status(201).json({message:"Server created",server});
+} 
--- apps/backend/src/controller/joinServer.ts
+++ apps/backend/src/controller/joinServer.ts
@@ -1,3 +1,20 @@
-export const joinServer=()=>{
-    console.log("Join Server Controller");
-}
+import { Request, Response } from "express";
+import CreateServer from "../models/createServer.model";
+
+export const joinServer=async(req:Request,res:Response)=>{
+      const data=req.body.ServerId;
+      if(!data){
+          return res.status(400).json({message:"Bad Request"});
+      };
+      const server=await CreateServer.findById(data);
+      if(!server){
+          return res.status(404).json({message:"Server not found"});
+      }
+      server.members.push({
+        userId: req.body.userId,
+        joinedAt: new Date(),
+        role: 'member'
+      });
+      await server.save();
+      return res.status(201).json({message:"Join the server",server});
+  } ;
